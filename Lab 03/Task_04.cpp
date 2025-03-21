83a55a44478f34d9fac5ae8db99d067f132ab1e2
Farjaz Kashif <92172779+FarjazKashif@users.noreply.github.com>
2025-03-22 03:39:46 +0500

    Update Task_04.cpp

--- Lab 03/Task_04.cpp
+++ Lab 03/Task_04.cpp
@@ -4,13 +4,19 @@ using namespace std;
 
 int main() {
     int len = 5;
-    float *arr[len], avg=0;
+    float arr[len], avg=0;
 
     for (int i=0; i<len; i++) {
-        cout << "Enter number " << i+1 << ": "; cin >> *arr[i];
-        avg += *arr[i];
+        cout << "Enter number " << i+1 << ": ";
+        cin >> *(arr + i);
+        /*
+        arr -> itself &arr[0]
+        1. // 100 + (0*4)
+        2. // 100 + (1*4)
+        */
+       avg += *(arr + i);
     }
-    cout << "Average: " << avg/len;
+    cout << "\nAverage = " << avg/len;
 
     return 0;
 }
